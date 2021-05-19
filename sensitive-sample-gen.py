9902275272414ad7bc70e6d14a2016ab01dd2f8c
zechengh <zcheustc@gmail.com>
2021-05-19 09:28:07 -0400

    validation

--- sensitive-sample-gen.py
+++ sensitive-sample-gen.py
@@ -18,30 +18,43 @@ def main():
 
     parser.add_argument('--input_dir_clean', type = str, default = 'data/VGGFace-Clean', help='Directory of input data')
     parser.add_argument('--input_dir_trojaned', type = str, default = 'data/VGGFace-Clean', help='Directory of trojaned data')
+    parser.add_argument('--label_file', type = str, default = 'data/names.txt', help='Labels')
     parser.add_argument('--model_clean', type = str, default = 'model/VGG-face-clean.pt', help='Clean model')
     parser.add_argument('--model_trojaned', type = str, default = 'model/VGG-face-trojaned.pt', help='Trojaned model')
-    parser.add_argument('--gpu', type = str, default = 'model/VGG-face-trojaned.pt', help='Trojaned model')
+    parser.add_argument('--gpu', dest='gpu', action='store_true', help='Use gpu')
+    parser.set_defaults(gpu=False)
+
     args = parser.parse_args()
 
 
     model = net.VGG16FaceNet()
     model.load_state_dict(torch.load(args.model_clean))
 
-    model_trojaned = net.VGG16FaceNet()
-    model_trojaned.load_state_dict(torch.load(args.model_trojaned))
+    #model_trojaned = net.VGG16FaceNet()
+    #model_trojaned.load_state_dict(torch.load(args.model_trojaned))
 
     if args.gpu:
-        model.cuda
+        model.cuda()
+        model_trojaned.cuda()
+
+    name_to_label, label_to_name = utils.get_label(args.label_file)
+
 
     for file_name in os.listdir(args.input_dir_clean):
         if file_name.startswith('.'):
             continue
 
+        name = file_name[:re.search(r"\d", file_name).start()-1]
+        label = name_to_label[name]
+
         img = utils.read_img(os.path.join(args.input_dir_clean, file_name))
         img = torch.unsqueeze(img, 0)
 
         logits = torch.squeeze(model(img))
-        print(file_name, torch.argmax(logits))
+        pred_label = torch.argmax(logits)
+        print(file_name, torch.argmax(logits), label)
+
+
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -10,3 +10,13 @@ def read_img(fname):
     img = img - averageImage
     img = np.moveaxis(img, -1, 0)
     return torch.tensor(img.astype(np.float32))
+
+def get_label(names_file):
+    name_to_index = {}
+    index_to_name = {}
+    with open(names_file, 'r') as f:
+        for i, name in enumerate(f):
+            name = name.strip('\n')
+            name_to_index[name] = i
+            index_to_name[i] = name
+    return name_to_index, index_to_name
