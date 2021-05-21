11f7f6688e3dc390c9c19083086dd438c1e84e7f
zechengh <zcheustc@gmail.com>
2021-05-21 13:08:46 -0400

    0.2

--- sensitive-sample-gen.py
+++ sensitive-sample-gen.py
@@ -127,7 +127,7 @@ def main():
     parser.add_argument('--label_file', type = str, default = 'data/names.txt', help='Labels')
     parser.add_argument('--model_clean', type = str, default = 'model/VGG-face-clean.pt', help='Clean model')
     parser.add_argument('--model_trojaned', type = str, default = 'model/VGG-face-trojaned.pt', help='Trojaned model')
-    parser.add_argument('--sensitivity_per_weight_th', type=float, default = 0.01, help='Threshold to determine if the generation is successful')
+    parser.add_argument('--sensitivity_per_weight_th', type=float, default = 0.2, help='Threshold to determine if the generation is successful')
     parser.add_argument('--gpu', dest='gpu', action='store_true', help='Use gpu')
     parser.set_defaults(gpu=False)
 
