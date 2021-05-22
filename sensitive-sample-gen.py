694cfc657e635cc593a2dc3c35f10a0f9265a979
zechengh <zcheustc@gmail.com>
2021-05-22 00:42:24 -0400

    TV loss

--- sensitive-sample-gen.py
+++ sensitive-sample-gen.py
@@ -90,7 +90,7 @@ def sensitive_sample_gen(
         loss_sensitivity = -torch.mean(df_dw[0]**2)
 
         loss_TV =  utils.TV(x)
-        loss = loss_sensitivity + loss_TV
+        loss = loss_sensitivity + 1e-4 * loss_TV
         #max_i = torch.argmax(softmax_out)
         #loss = 0
         #for topi in torch.topk(softmax_out, 1)[1]:
