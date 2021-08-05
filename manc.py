f790448aadd17618de1854d6e46c9eab2fcb5895
zechengh <zcheustc@gmail.com>
2021-08-05 18:51:24 -0400

    debug

--- manc.py
+++ manc.py
@@ -37,6 +37,8 @@ def manc(candidates, model, n_samples):
         activation_maps.append(activation)
 
     candidates_activation = torch.cat(activation_maps, axis=0)
+    print([torch.sum(candidates_activation[i]) for i in range(candidates_activation.size()[0])])
+
     current_union_map = torch.zeros(size=candidates_activation[0].size(), dtype=torch.bool)
     if candidates_activation.is_cuda:
         current_union_map = current_union_map.cuda()
