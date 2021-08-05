f30fc124b9789a116556dc63a210a4baa40a3006
zechengh <zcheustc@gmail.com>
2021-08-05 13:27:51 -0400

    debuf

--- manc.py
+++ manc.py
@@ -48,6 +48,8 @@ def manc(candidates, model, n_samples):
 
     for idx in range(n_samples):
         n_joint_activated = [(torch.sum(current_union_map | candidates_activation[i]), i) for i in remaining]
+        print(n_joint_activated)
+
         current_selected_idx = np.max(n_joint_activated)[1]
 
         print('selected', selected)
