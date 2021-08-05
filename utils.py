abdb2bc05c2f54f8079d45d34f3a5686ce58e481
zechengh <zcheustc@gmail.com>
2021-08-05 11:34:38 -0400

    debug

--- utils.py
+++ utils.py
@@ -257,7 +257,7 @@ def pred_diff(candidates, model_clean, model_trojaned, verbose=False):
     n_total = 0.0
     n_diff = 0.0
 
-    data_loader = torch.utils.data.DataLoader(candidates, batch_size=32)
+    data_loader = torch.utils.data.DataLoader(candidates, batch_size=64)
 
     for batch_idx, candidate in enumerate(data_loader):
 
