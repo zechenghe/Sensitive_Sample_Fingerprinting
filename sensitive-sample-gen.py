85a6e9d09b5b1acfedb4eb3c01b38f93103eb071
zechengh <zcheustc@gmail.com>
2021-08-17 17:13:47 -0400

    readme

--- sensitive-sample-gen.py
+++ sensitive-sample-gen.py
@@ -97,6 +97,7 @@ def main():
     parser.set_defaults(early_stop=False)
     parser.add_argument('--sensitivity_per_weight_th', type=float, default = 5e-4, help='Threshold to determine if the generation is successful')
     parser.add_argument('--gpu', dest='gpu', action='store_true', help='Use gpu')
+    parser.add_argument('--n_sensitive_samples', type=int, default = 100, help='Number of sensitive samples to generate')
     parser.set_defaults(gpu=False)
 
     parser.add_argument('--nosanity_check', dest='sanity_check', action='store_false', help='Sanity check. Evaluate models before sample generation')
@@ -147,6 +148,7 @@ def main():
     sensitivity_per_weight_same = []
     activated_neurons_same = []
 
+    n_generated = 0
     for file_name in os.listdir(args.input_dir_clean):
         x = utils.read_img(os.path.join(args.input_dir_clean, file_name))
         x_origin = x.clone().detach().cpu().numpy()
@@ -177,6 +179,7 @@ def main():
         print(snr)
 
         if (sensitivity_per_weight > args.sensitivity_per_weight_th) and snr > 0:
+            n_generated += 1
             diff = utils.is_diff(logits_clean, logits_trojaned, mode='topk', k=1)
 
             if diff:
@@ -198,6 +201,10 @@ def main():
         print(f"Sensitivity per weight, diff {np.mean(sensitivity_per_weight_diff) if len(sensitivity_per_weight_diff) > 0 else 0}, same {np.mean(sensitivity_per_weight_same)if len(sensitivity_per_weight_same) > 0 else 0}")
         print(f"Number of activated neurons, diff {np.mean(activated_neurons_diff) if len(activated_neurons_diff) > 0 else 0}, same {np.mean(activated_neurons_same) if len(activated_neurons_same) else 0}")
         print("#############")
+
+        if n_generated >= args.n_sensitive_samples:
+            print(f"Success: {n_generated} sensitive samples generated.")
+            return
 
 if __name__ == '__main__':
     main()
